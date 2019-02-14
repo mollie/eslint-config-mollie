27f592aecb73d5aef24da2f9829007f9bc5c1f49
Vernon de Goede <vernon@mollie.com>
2019-02-14 09:48:00 +0100

    Change order of extends for eslint-config-react

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -3,7 +3,7 @@ module.exports = {
    * Extending works from right to left. The rules inside the package on the right will always "win"
    * from the rules that are defined in the packages on the left side.
    */
-  extends: ["@mollie/eslint-config-base", "react-app"],
+  extends: ["react-app", "@mollie/eslint-config-base"],
   plugins: ["jsx-a11y", "react-hooks"],
   rules: {
     "jsx-a11y/no-static-element-interactions": "warn",
