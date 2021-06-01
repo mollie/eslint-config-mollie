ccdda7f96ddf286ef3b5cd33ef5293b69b7083a8
Philipp Rappold <philipp.rappold@mollie.com>
2021-06-01 12:00:53 +0200

    Bump deps

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   /**
-   * Extending works from right to left. The rules inside the package on the right will always "win"
-   * from the rules that are defined in the packages on the left side.
+   * Extending works from right to left. The rules inside the package
+   * on the right will always "win" from the rules that are defined in
+   * the packages on the left side.
    */
   extends: [
     "plugin:@typescript-eslint/recommended",
@@ -10,6 +11,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/interface-name-prefix": "off"
+    "@typescript-eslint/interface-name-prefix": "off",
   },
 };
